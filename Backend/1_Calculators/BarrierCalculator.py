d00e9e23c8883883fa24d6aea877a30068257599
jopawi7 <60352040+jopawi7@users.noreply.github.com>
2025-10-30 16:59:05 +0100

    main.py and basic structure

---
+++ Backend/1_Calculators/BarrierCalculator.py
@@ -0,0 +1,31 @@
+
+
+
+def calculateOptionValue(data):
+    # Expected Output values
+    price = 0
+    delta = 0
+    gamma = 0
+    rho = 0
+    theta = 0
+    vega = 0
+
+    # How to call the values out of the data object:
+    print(f"Option type: {data['type']}")
+    print(f"Exercise style: {data['exercise_style']}")
+    print(f"Start date: {data['start_date']}")
+    print(f"Start time: {data['start_time']}")
+    print(f"Expiration date: {data['expiration_date']}")
+    print(f"Expiration time: {data['expiration_time']}")
+    print(f"Strike: {data['strike']}")
+    print(f"Stock price: {data['stock_price']}")
+    print(f"Volatility: {data['volatility']}")
+    print(f"Interest rate: {data['interest_rate']}")
+    print(f"Dividend yield: {data['dividend_yield']}")
+
+    # TODO: Logic to calculate OptionValue here or method to calculate option value
+
+    return {
+        "symbol": "placeholder", "theoretical_price": round(price, 4), "delta": round(delta, 4),
+        "gamma": round(gamma, 6), "rho": round(rho, 4), "theta": round(theta, 4), "vega": round(vega, 4)
+    }
---
+++ Backend/1_Calculators/main.py
@@ -0,0 +1,72 @@
+import json
+import EuropeanAmericanCalculator as EuropeanAmericanCalculator
+import AsianCalculator as AsianCalculator
+import BinaryCalculator as BinaryCalculator
+import BarrierCalculator as BarrierCalculator
+
+# ---------------------------------------------------------
+# Filename: main.py
+# Author: Jonas Patrick Witzel
+# Created: 2025-10-30
+# Description: Reads input.json, Calculates the Corresponding results, Writes it into output.json
+# ---------------------------------------------------------
+
+if __name__ == '__main__':
+    print("Option calculation starts...")
+    print("")
+
+    # Path to Input in input.json... :)
+    with open('../0_Input/input.json', 'r') as f:
+        input_obj = json.load(f)
+
+    # How to call the Variables
+    #print("Input data loaded successfully!")
+    #print(f"Option type: {input_obj['type']}")
+    #print(f"Exercise style: {input_obj['exercise_style']}")
+    #print(f"Start date: {input_obj['start_date']}")
+    #print(f"Start time: {input_obj['start_time']}")
+    #print(f"Expiration date: {input_obj['expiration_date']}")
+    #print(f"Expiration time: {input_obj['expiration_time']}")
+    #print(f"Strike: {input_obj['strike']}")
+    #print(f"Stock price: {input_obj['stock_price']}")
+    #print(f"Volatility: {input_obj['volatility']}")
+    #print(f"Interest rate: {input_obj['interest_rate']}")
+    #print(f"Dividend yield: {input_obj['dividend_yield']}")
+
+    # Select the corresponding calculator and calculate results and put into output.json
+    output_obj = None
+
+    match input_obj['exercise_style'].lower():
+        case "european" | "american":
+            output_obj = EuropeanAmericanCalculator.calculateOptionValue(input_obj)
+        case "asian":
+            output_obj = AsianCalculator.calculateOptionValue(input_obj)
+        case "binary":
+            output_obj = BinaryCalculator.calculateOptionValue(input_obj)
+        case "barrier":
+            output_obj = BarrierCalculator.calculateOptionValue(input_obj)
+        case _:
+            raise ValueError("Invalid exercise style")
+
+    # Print Results from Calulation
+    if output_obj is None:
+        print("Something went wrong!")
+    else:
+        print("")
+        print("----- Result Summary -----")
+        print(f"Symbol: {output_obj.get('symbol', 'N/A')}")
+        print(f"Theoretical Price: {output_obj.get('theoretical_price', 'N/A')}")
+        print(f"Delta: {output_obj.get('delta', 'N/A')}")
+        print(f"Gamma: {output_obj.get('gamma', 'N/A')}")
+        print(f"Rho: {output_obj.get('rho', 'N/A')}")
+        print(f"Theta: {output_obj.get('theta', 'N/A')}")
+        print(f"Vega: {output_obj.get('vega', 'N/A')}")
+
+    # Safe the output
+    try:
+        with open('../2_Output/output.json', "w") as f:
+            json.dump(output_obj, f, indent=2)
+        print(f"Results successfully written to: output.json")
+    except Exception as e:
+         print(f"An error occured while calculating or saving the output: {e}")
+
